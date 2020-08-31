a04aa7592bdd071657898c81dd76b31d28632e09
Meyanis95 <b00694485@essec.edu>
2020-08-31 12:04:00 +0200

    User Devise generated

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def create
+  end 
+
+  def show
+  end
+
+  def update
+  end 
+
+  def destroy
+  end
+end
