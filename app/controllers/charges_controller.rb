73298b63602018cd0235730e6c01e310207b2a71
donatellalnrs <donatella.gonzalezdelinares@essec.edu>
2020-09-02 19:48:25 +0200

    stripe plug in v2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,7 +2,7 @@ class ChargesController < ApplicationController
   def new
     @cart = current_user.cart
     @item_carts = ItemCart.where(cart: @cart)
-    @amount = @item_carts.reduce(0){|sum, ic| sum + ic.item.price}.round(2)*100
+    @amount = @item_carts.reduce(0){|sum, ic| sum + ic.item.price}.round(2)
   end
 
   def create
@@ -16,14 +16,28 @@ class ChargesController < ApplicationController
     })
 
     charge = Stripe::Charge.create({
-      customer: current_user.id,
+      customer: customer.id,
       amount: @amount.to_i,
       description: 'Rails Stripe customer',
       currency: 'usd',
     })
+    cart_empty
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  private
+  def cart_empty
+    @order = Order.create(user: current_user)
+    @cart = current_user.cart
+    item_carts = ItemCart.where(cart: @cart)
+    item_carts.each do |item_cart|
+      ItemOrder.create(item: item_cart.item, order: @order)
+      item_cart.destroy
+    end
+  end
+
+
 end
